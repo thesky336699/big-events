3c03c902bb8a9fbc9816581745472966e9145c2a
mrzhang <zhangyanzhe07@163.com>
2020-10-06 22:02:54 +0800

    储存

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,7 +8,7 @@ $(function() {
         $('.reg-box').hide();
     })
 
-    // 设置表单验证
+    // 设置表单验证 layui 中方法
     var form = layui.form;
     form.verify({
         username: function(value, item) { //value：表单的值、item：表单的DOM对象
@@ -34,7 +34,49 @@ $(function() {
             if (value !== psd) {
                 return '两次输入不一致'
             }
-
         }
     });
+    var layer = layui.layer;
+    // 设置ajax请求 注册  点击事件
+    $('#form-reg').on('submit', function(e) {
+            // 阻止表单默认行为
+            e.preventDefault();
+            // console.log(222);
+            var data = {
+                username: $('#form-reg [name=username]').val(),
+                password: $('#form-reg [name=password]').val()
+            }
+            $.ajax({
+                type: "POST",
+                url: "http://ajax.frontend.itheima.net/api/reguser",
+                data: data,
+                // dataType: "dataType",
+                success: function(res) {
+                    if (res.status !== 0) return layer.msg(res.message, { icon: 6 });
+                    layer.msg('注册成功', { icon: 6 });
+                    // 成功之后返回登录页面  自动点击事件
+                    $('#link_login').click();
+                }
+            });
+
+        })
+        // 设置ajax请求 登录  点击事件
+    $('#form-login').on('submit', function(e) {
+        //阻止表单默认行为
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://ajax.frontend.itheima.net/api/login",
+            data: $(this).serialize(),
+            // dataType: "dataType",
+            success: function(res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                layer.msg('登录成功');
+                // token 数据保存到本地
+                localStorage.setItem('token', res.token);
+                //href:设置或者获取地址栏信息路
+                location.href = '/index.html';
+            }
+        });
+    })
 })
